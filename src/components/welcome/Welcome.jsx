a5ea4e5db30659c52163b4e574db62f778f1671d
ccaioadriano <caiobhadriano@gmail.com>
2023-11-29 12:55:48 -0300

    Criando context para aplicacao

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Quiz from "../../img/quiz.svg";
 import "./Welcome.css";
-
+import { QuizContext } from "../../context/quiz.jsx";
+import { useContext } from "react";
 export default function Welcome() {
+  const quizState = useContext(QuizContext);
+
+  console.log(quizState.name);
   return (
     <div id="welcome">
       <h2>Seja bem vindo.</h2>
