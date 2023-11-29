e466da016596282d85c9fa96249d3fe93393346b
ccaioadriano <caiobhadriano@gmail.com>
2023-11-29 13:38:01 -0300

    Criando o reducer para gerenciar ações do usuario

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -3,15 +3,15 @@ import Quiz from "../../img/quiz.svg";
 import "./Welcome.css";
 import { QuizContext } from "../../context/quiz.jsx";
 import { useContext } from "react";
-export default function Welcome() {
-  const quizState = useContext(QuizContext);
 
-  console.log(quizState.name);
+export default function Welcome() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div id="welcome">
       <h2>Seja bem vindo.</h2>
       <p>Clique no botão abaixo para começar.</p>
-      <button>Iniciar</button>
+      <button onClick={() => {dispatch({type: "CHANGE_STATE"})}}>Iniciar</button>
       <img src={Quiz} alt="welcome" />
     </div>
   );
